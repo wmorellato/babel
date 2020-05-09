51efb24eb0dfc2ab55587a80e0e6f9266919047c
Weslley Morellato <morellato.weslley@gmail.com>
2020-05-09 14:39:30 -0300

    Export to docx

---
+++ src/exporter.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+const path = require('path');
+const JSZip = require('jszip');
+const Docxtemplater = require('docxtemplater');
+const util = require('util');
+const Errors = require('./errors');
+const resources = require('./vscode/resource-manager');
+
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+
+const FileExtension = {
+  DOCX: '.docx',
+  PDF: '.pdf',
+}
+
+const Template = {
+  SHUNN_MANUSCRIPT: {
+    name: 'Shunn Manuscript Format (Story Manuscript)',
+    url: 'https://www.shunn.net/format/templates.html',
+    path: 'resources/templates/shunn.docx',
+    fields: ['author', 'author_surname', 'title_keyword', 'address', 'city_postal', 'phone', 'email', 'title', 'word_count', 'content'],
+    fileFormat: FileExtension.DOCX,
+  },
+};
+
+class Exporter {
+  /**
+   * 
+   * @param {*} outputPath 
+   * @param {Object} storyDescriptor a story describing the story
+   */
+  constructor(outputPath, storyDescriptor) {
+    this.outputPath = outputPath;
+    this.storyDescriptor = storyDescriptor;
+  }
+
+  /**
+   * Validate the story descriptor passed, throwing an Error in case any field
+   * required by the template is missing.
+   * @param {Template} template Template enum item describing one of the available templates
+   * @param {Boolean} acceptMissing if the export operation should continue even with a field missing
+   * @throws {Errors.EXPORT_INVALID_TEMPLATE_ERROR} if the provided template is invalid
+   * @throws {Errors.EXPORT_MISSING_FIELD_ERROR} if any required field is missing
+   */
+  validateDescriptor(template, acceptMissing) {
+    // improve this checking
+    if (!template.name || !template.url || !template.path || !template.fields) {
+      throw new Error(Errors.EXPORT_INVALID_TEMPLATE_ERROR);
+    }
+
+    template.fields.forEach((key) => {
+      if (!this.storyDescriptor[key]) {
+        if (!acceptMissing) {
+          // improve this to show which field is missing
+          throw new Error(Errors.EXPORT_MISSING_FIELD_ERROR);
+        }
+      }
+    });
+  }
+
+  /**
+   * Load the template binary contents.
+   * @param {Template} template Template enum item describing one of the available templates
+   */
+  loadTemplateFile(template) {
+    try {
+      // trying to load the template using resources
+      const absolutePath = resources.getResourcePathByName(template.path).fsPath;
+
+      return readFilePromise(absolutePath);
+    } catch (e) {
+      throw new Error(Errors.EXPORT_TEMPLATE_LOADING_ERROR);
+    }
+  }
+
+  /**
+   * Replace the keys in the template by its corresponding fields in the
+   * story descriptor.
+   * @param {JSZip} plainTemplate JSZip object with template content inflated
+   */
+  fillTemplate(plainTemplate) {
+    return new Promise((resolve) => {
+      const doc = new Docxtemplater(plainTemplate);
+
+      doc.setData(this.storyDescriptor);
+      doc.render();
+      
+      const zipContent = doc.getZip().generate({type: 'nodebuffer', compression: "DEFLATE"});
+      resolve(zipContent);
+    });
+  }
+
+  /**
+   * Function to export to .docx files.
+   * @param {Template} template one of Template enum items
+   */
+  exportToDocx(template) {
+    return this.loadTemplateFile(template)
+      .then((data) => {
+        const plainTemplate = new JSZip(data).load(data);
+        return this.fillTemplate(plainTemplate);
+      })
+      .then((zippedTemplate) => {
+        const outputFile = path.join(this.outputPath, this.storyDescriptor.title + template.fileFormat)
+        return writeFilePromise(outputFile, zippedTemplate);
+      })
+      .catch((e) => {
+        throw new Error(e);
+      });
+  }
+
+  /**
+   * Call the chain of functions to export the story to the desired
+   * template.
+   * @param {Template} template to which template the story will be exported
+   */
+  export(template) {
+    this.validateDescriptor(template, false);
+
+    switch (template.fileFormat) {
+      case FileExtension.DOCX:
+        return this.exportToDocx(template);
+    }    
+  }
+}
+
+module.exports = {
+  Exporter,
+  Template,
+};
---
+++ test/suite/exporter.test.js
@@ -0,0 +1,41 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const resources = require('../../src/vscode/resource-manager');
+const { Exporter, Template } = require('../../src/exporter');
+const { expect } = require('chai');
+const Errors = require('../../src/errors');
+
+suite.only('exporter tests', function () {
+  const story_descriptor = {
+    author: 'Jorge Luis Borges',
+    author_surname: 'Borges',
+    title_keyword: 'Babel',
+    address: 'Buenos Aires, Argentina',
+    city_postal: '642-2054',
+    phone: '123456',
+    email: 'borges@uqbar.com',
+    title: 'A Biblioteca de Babel',
+    word_count: 198798,
+    content: 'O universo (que outros chamam a Biblioteca) compõe-se de um número indefinido, e talvez infinito, de galerias hexagonais, com vastos poços de ventilação no centro, cercados por balaustradas baixíssimas. De qualquer hexágono, vêem-se os andares inferiores e superiores: interminavelmente. A distribuição das galerias é invariável. Vinte prateleiras, em cinco longas estantes de cada lado, cobrem todos os lados menos dois; sua altura, que é a dos andares, excede apenas a de um bibliotecário normal. Uma das faces livres dá para um estreito vestíbulo, que desemboca em outra galeria, idêntica à primeira e a todas. A esquerda e à direita do vestíbulo, há dois sanitários minúsculos. Um permite dormir em pé; outro, satisfazer as necessidades físicas. Por aí passa a escada espiral, que se abisma e se eleva ao infinito. No vestíbulo há um espelho, que fielmente duplica as aparências. Os homens costumam inferir desse espelho que a Biblioteca não é infinita (se o fosse realmente, para que essa duplicação ilusória?), prefiro sonhar que as superfícies polidas representam e prometem o infinito… A luz procede de algumas frutas esféricas que levam o nome de lâmpadas. Há duas em cada hexágono: transversais. A luz que emitem é insuficiente, incessante.',
+  };
+  const outputPath = fs.mkdtempSync(path.join(os.tmpdir(), 'bab-'));
+
+  this.beforeAll(function () {
+    resources.init({
+      extensionPath: path.join(__dirname, '..', '..'),
+    });
+  });
+
+  test('should throw for missing field', function () {
+    expect(() => { new Exporter(outputPath, {}).export(Template.SHUNN_MANUSCRIPT) }).to.throw(Errors.EXPORT_MISSING_FIELD_ERROR);
+  });
+
+  test('should create output docx', async function () {
+    const exporter = new Exporter(outputPath, story_descriptor);
+    await exporter.export(Template.SHUNN_MANUSCRIPT);
+
+    const docxPath = path.join(outputPath, story_descriptor.title + '.docx');
+    expect(fs.existsSync(docxPath)).to.be.equal(true);
+  });
+});
